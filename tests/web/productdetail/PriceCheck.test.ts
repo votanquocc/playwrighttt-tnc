1e338601dc727263a892df8d589c65405b6401f2
votanquocc <quoc1610@outlook.com>
2025-10-21 13:04:57 +0700

    update and optimize

--- tests/web/productdetail/PriceCheck.test.ts
+++ tests/web/productdetail/PriceCheck.test.ts
@@ -1,11 +1,7 @@
 import {basetest, expect} from '../../../fixtures/BaseTest';
-import { HomePage } from '../../../src/pages/HomePage';
-import { ProductDetailPage } from '../../../src/pages/ProductDetailPage';
 
-basetest('PriceCheck', async ({page}) => {
-    const homePage = new HomePage(page);
+basetest('PriceCheck', async ({page , productDetailPage, homePage}) => {
     await homePage.navigateToFirstProductDetail();
-    const productDetailPage = new ProductDetailPage(page);
     await page.waitForTimeout(2000);
     // Gọi hàm và nhận kết quả trả về
     const priceData = await productDetailPage.calculateAndVerifyDiscount();
